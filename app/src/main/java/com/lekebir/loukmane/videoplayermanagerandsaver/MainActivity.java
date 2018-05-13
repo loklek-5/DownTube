fc4a1af981efb619b6eef3112fddd7c336375ba5
KiMOS <kimos@MacBook-Pro-de-LOKMANE.local>
2018-05-13 22:10:11 +0200

    First commit

---
+++ app/src/main/java/com/lekebir/loukmane/videoplayermanagerandsaver/MainActivity.java
@@ -0,0 +1,60 @@
+package com.lekebir.loukmane.videoplayermanagerandsaver;
+
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+//import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+    private Toolbar mToolbar;
+    public Button ytButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mToolbar = findViewById(R.id.nav_action);
+        setSupportActionBar(mToolbar);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout,R.string.open,R.string.close);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        initytActivity();
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    public void initytActivity(){
+        ytButton = (Button) findViewById(R.id.ytButton);
+        ytButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent yt = new Intent(MainActivity.this,YTActivity.class);
+                startActivity(yt);
+            }
+        });
+
+    }
+}
