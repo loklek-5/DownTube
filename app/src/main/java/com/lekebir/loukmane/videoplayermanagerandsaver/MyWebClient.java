591ffad6eab2ecad3e8bc0ff5637d19ba6bddae8
loklek-5 <lukatoni1992@gmail.com>
2018-05-15 23:33:06 +0200

    get source

--- app/src/main/java/com/lekebir/loukmane/videoplayermanagerandsaver/MyWebClient.java
+++ app/src/main/java/com/lekebir/loukmane/videoplayermanagerandsaver/MyWebClient.java
@@ -3,12 +3,23 @@ import android.webkit.WebViewClient;
 import android.webkit.WebView;
 import android.graphics.Bitmap;
 import android.util.Log;
+import android.widget.Toast;
+import android.content.Context;
+
 public class MyWebClient extends WebViewClient
     {
+        private Context context;
+
+        public MyWebClient(Context ctx) {
+            context = ctx;
+        }
         @Override
         public void onPageStarted(WebView view, String url, Bitmap favicon) {
                 super.onPageStarted(view, url, favicon);
-                Log.d("WebView", "your current url when webpage loading.." + url);
+                Log.d("WebView", "your current url when webpage loading.. start" + url);
+                //Toast.makeText(context, url, Toast.LENGTH_SHORT).show();
+
+
         }
         @Override
         public void onPageFinished(WebView view, String url) {
@@ -18,7 +29,7 @@ public class MyWebClient extends WebViewClient
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             // TODO Auto-generated method stub
-
+            //Toast.makeText(context, url, Toast.LENGTH_SHORT).show();
             view.loadUrl(url);
             return true;
 
